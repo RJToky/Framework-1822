99473c94e626a9178ca9160b90f7b9a4d36630bc
RJToky <tokyrakotoarivony5@gmail.com>
2023-03-14 11:50:13 +0300

    Add allClassName

--- src/main/java/etu1822/framework/servlet/FrontServlet.java
+++ src/main/java/etu1822/framework/servlet/FrontServlet.java
@@ -6,19 +6,39 @@ import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Objects;
 
 @WebServlet(name = "FrontServlet", value = "/")
 public class FrontServlet extends HttpServlet {
     HashMap<String, Mapping> mappingUrls;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+
+        ClassLoader classLoader = getServletContext().getClassLoader();
+        URI uri = null;
+        try {
+            uri = Objects.requireNonNull(classLoader.getResource("")).toURI();
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+        File file = new File(uri);
+    }
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) {
         try {
             PrintWriter out = response.getWriter();
             String url = Util.getURI(request);
             out.println(url);
+
         } catch (Exception ignored) { }
     }
 
--- src/main/java/etu1822/framework/utility/Util.java
+++ src/main/java/etu1822/framework/utility/Util.java
@@ -2,6 +2,12 @@ package etu1822.framework.utility;
 
 import jakarta.servlet.http.HttpServletRequest;
 
+import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class Util {
     public  static String getURI(HttpServletRequest request) {
         String requestURI = request.getRequestURI();
@@ -13,4 +19,26 @@ public class Util {
         }
         return rep.toString();
     }
+
+    public static void getAllClassName(File path, ArrayList<String> allClassName) {
+        String className;
+
+        for(File file : Objects.requireNonNull(path.listFiles())) {
+            if (file.isDirectory()) {
+                getAllClassName(file, allClassName);
+            } else {
+                className = file.getPath().replace(".class", "");
+
+                if (className.contains("\\")) {
+                    className = className.split("\\\\WEB-INF\\\\classes\\\\")[1];
+                    className = className.replace("\\", ".");
+                } else {
+                    className = className.split("/WEB-INF/classes/")[1];
+                    className = className.replace("/", ".");
+                }
+                allClassName.add(className);
+            }
+        }
+    }
+
 }
